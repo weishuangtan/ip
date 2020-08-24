7497fe8224d49075bfe5b79d0f2b9ffd5336d6df
DESKTOP-55NKBTR\Wei Shuang <wilyntan@gmail.com>
2020-08-24 15:09:50 +0800

    Added string array to store text entered by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,16 +18,19 @@ public class Duke {
         System.out.println("Hello! I'm Julia\nIs there anything I can do for you today?");
         printLine();
         int exit = 0;
+        int i = 0;
+        String[] inputs = new String[100];
         while (exit == 0){
-            String input = sc.nextLine();
+            inputs[i] = sc.nextLine();
             printLine();
-            if (input.equals("bye")){
+            if (inputs[i].equals("bye")){
                 exit = 1;
             }
             else {
-                System.out.println(input);
+                System.out.println("Okay, I have added the following into the list for you!\n" + "-> " + inputs[i]);
                 printLine();
             }
+            i++;
         }
 
         System.out.println("Goodbye! I look forward to seeing you the next time!");
