8ed7165b82b2837af6198af734b8a0ccfdb773d5
DESKTOP-55NKBTR\Wei Shuang <wilyntan@gmail.com>
2020-08-24 13:15:35 +0800

    Added Greet, Echo and Exit function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,6 +12,19 @@ public class Duke {
         printLine();
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         printLine();
+        int exit = 0;
+        while (exit == 0){
+            String input = sc.nextLine();
+            printLine();
+            if (input.equals("bye")){
+                exit = 1;
+            }
+            else {
+                System.out.println(input);
+                printLine();
+            }
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         printLine();
     }
