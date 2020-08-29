02759084c9b8367068e57b5e157ee5fc92019c59
DESKTOP-55NKBTR\Wei Shuang <wilyntan@gmail.com>
2020-08-29 11:01:16 +0800

    Added Deadline task as sub-class and refactor code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,12 +6,12 @@ public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         initialiseJulia();
-        ArrayList <Task> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
         State state = State.running;
         while (state == State.running) {
             String input = in.nextLine();
             printLine();
-            state = analyseInput(input,tasks);
+            state = analyseInput(input, tasks);
             printLine();
         }
     }
@@ -24,7 +24,6 @@ public class Duke {
      * @return state of chat bot
      */
     public static State analyseInput(String input, ArrayList<Task> tasks) {
-        String[] words = input.split(" ");
         if (input.equals("bye")) {
             // Exits the programme
             printGoodbye();
@@ -32,39 +31,23 @@ public class Duke {
         } else if (input.equals("list")) {
             // Lists the tasks
             printList(tasks);
-
         } else if (input.startsWith("done")) {
             // Checks task off the list
-            try {
-                checkTaskOff(words[1],tasks);
-            } catch (Exception e) {
-                printNotFound();
-            }
-        } else if (input.startsWith("todo ")){
-            // Add tasks without any date/time attached
-            addToDo(input,tasks);
-        }else {
+            checkTaskOff(input, tasks);
+        } else if (input.startsWith("todo")) {
+            // Adds to do tasks
+            addToDo(input, tasks);
+        } else if (input.startsWith("deadline")) {
+            // Adds deadline tasks
+            addDeadline(input, tasks);
+        } else {
             // Adds tasks into the list
-            addTask(input,tasks);
+            addTask(input, tasks);
         }
         return State.running;
 
     }
 
-    /**
-     * Tries checking given task off the list
-     *
-     * @param word task number given by user
-     * @param tasks list of tasks
-     */
-    public static void checkTaskOff(String word, ArrayList<Task> tasks){
-        int complete = Integer.parseInt(word);
-        if (complete <= tasks.size()) {
-            tasks.get(complete - 1).markAsDone();
-        } else {
-            printNotFound();
-        }
-    }
 
     /**
      * Prints starting page of Julia the chat bot
@@ -96,15 +79,22 @@ public class Duke {
     /**
      * Prints goodbye message
      */
-    public static void printGoodbye(){
+    public static void printGoodbye() {
         System.out.println("Goodbye! I look forward to seeing you the next time!");
     }
 
     /**
      * Prints error message
      */
-    public static void printNotFound(){
+    public static void printNotFound() {
         System.out.println("No such task found, try again?");
+    }
+
+    /**
+     * Prints incorrect format message
+     */
+    public static void printIncorrectFormat(){
+        System.out.println("Incorrect format, please try again!");
     }
 
     /**
@@ -118,7 +108,7 @@ public class Duke {
         } else {
             System.out.println("Here is the collated list of your tasks:");
             int i = 0;
-            for (Task task: tasks) {
+            for (Task task : tasks) {
                 System.out.println("(" + (i + 1) + ") " + task);
                 i++;
             }
@@ -126,15 +116,25 @@ public class Duke {
         System.out.println("Feel free to continue adding to the list! :)");
     }
 
-    /** Adds given task into the list of tasks
+    /**
+     * Tries checking given task off the list
      *
-     * @param input task description
+     * @param input user's input
      * @param tasks list of tasks
      */
-    public static void addTask(String input, ArrayList<Task> tasks){
-        Task item = new Task(input);
-        tasks.add(item);
-        System.out.println("Okay, I have added the following into the list for you!\n" + "-> " + input);
+    public static void checkTaskOff(String input, ArrayList<Task> tasks) {
+        String[] words = input.split(" ");
+        try {
+            int complete = Integer.parseInt(words[1]);
+            if (complete <= tasks.size()) {
+                tasks.get(complete - 1).markAsDone();
+            } else {
+                printNotFound();
+            }
+        }
+        catch (Exception e){
+            printIncorrectFormat();
+        }
     }
 
     /**
@@ -143,11 +143,53 @@ public class Duke {
      * @param input user's input
      * @param tasks list of tasks
      */
-    public static void addToDo(String input, ArrayList<Task> tasks){
-        String task = input.substring(5,input.length()-5);
-        ToDo item = new ToDo(task);
+    public static void addToDo(String input, ArrayList<Task> tasks) {
+        try {
+            String task = input.substring(5);
+            ToDo item = new ToDo(task);
+            tasks.add(item);
+            System.out.println("Okay, I have added the following into the list for you!\n" + "-> " + item);
+        }
+        catch (Exception e){
+            printIncorrectFormat();
+        }
+    }
+
+    /**
+     * Add Deadline task into the list of tasks
+     *
+     * @param input user's input
+     * @param tasks list of tasks
+     */
+    public static void addDeadline(String input, ArrayList<Task> tasks) {
+        try{
+            String task = input.substring(9);
+            String[] descriptionAndBy = task.split(" /p ");
+
+            Deadline item = new Deadline(descriptionAndBy[0], descriptionAndBy[1]);
+            tasks.add(item);
+            System.out.println("Okay, I have added the following into the list for you!\n" + "-> " + item);
+
+        }
+        catch (Exception e){
+            printIncorrectFormat();
+        }
+
+    }
+
+    /**
+     * Adds given task into the list of tasks
+     *
+     * @param input task description
+     * @param tasks list of tasks
+     */
+    public static void addTask(String input, ArrayList<Task> tasks) {
+
+        Task item = new Task(input);
         tasks.add(item);
-        System.out.println("Okay, I have added the following into the list for you!\n" + "-> " + item);
+        System.out.println("Okay, I have added the following into the list for you!\n" + "-> " + input);
     }
 }
+
+
 
